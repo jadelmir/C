f5f2af6c638cf615ee90e26650093e859a8ffd83
Jad El mir <jadelmir@Jads-MacBook-Pro.local>
2019-01-25 08:23:28 -0500

    f6 and f7 exercises

--- F7.c
+++ F7.c
@@ -2,27 +2,31 @@
 
 int main(int argc, char const *argv[])
 {
-    int NumberOfAst=1;
+    int NumberOfAst=0;
     int NumberOfSpaces=0;
-    int NumberOfCols;
-    int Rows ;
+    int NumberOfCols=0;
+    int Rows =0;
+    int numberOfPyramids=0;
     // Scan number of rows from input 
+    printf("please enter number of pyramids");
+    scanf("%d",&numberOfPyramids);
     printf("Hello please enter number of rows");
     scanf("%d",&Rows);
 
     NumberOfCols = Rows*2-1;
+
     
-    for(int i = 0; i < Rows; i++) // printing the rows
-    {
-        NumberOfAst = i*2 + 1;
+    for(int i = 0; i < Rows; i++)
+    {        NumberOfAst = i*2 + 1;
         NumberOfSpaces =  (NumberOfCols - NumberOfAst) ;
+        printf("\n");
+        for(int z = 0; z < numberOfPyramids; z++)
+         {
     int numberOfPrintedAst = 0;
-    
-        
-         for(int j = 0; j < NumberOfCols; j++)// printing each column
-        {
-            
-            if(NumberOfSpaces/2> j){
+
+             for(int j = 0; j < NumberOfCols; j++)
+             {
+                        if(NumberOfSpaces/2> j){
 
                 printf(" ");
             }else if(NumberOfAst > numberOfPrintedAst){
@@ -33,12 +37,12 @@ int main(int argc, char const *argv[])
                 printf(" ");
 
             }
+            }
+            
         }
-        printf("\n");
         
     }
     
 
     return 0;
 }
-
