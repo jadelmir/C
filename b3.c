9897be49063117964f4c17c3c48c09feca920af4
Jad El Mir <cs60901@y30.indstate.edu>
2019-02-08 08:44:27 -0500

    ctrcpy

---
+++ b3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+ int main() {
+
+   int a[100];
+
+   strcpy(a,"hello");
+   printf("%s\n",a );
+   int m1 ;
+   int n ;
+   int n1;
+   int n2 ;
+
+   n1=m1&n;
+   n2 = m2&n;
+
+}
--- b6.c
+++ b6.c
@@ -3,11 +3,11 @@ int count10(int);
 int main(){
     int n ;
     scanf("%d",&n);
-    // int CheckCount = count10(n); 
+    // int CheckCount = count10(n);
     printf("COUNT is %d",count10(n) );
 }
 
-// count every 1 and 0 bits
+// count every 010 bits
 int count10(int n){
     int count =0 ;
     //because it's look like (111) in bits (000&111)=000 (010&&111)=010
@@ -16,10 +16,10 @@ int count10(int n){
         {
             printf("In i = %d ",i);
             printf("N is = %d  and nmask is = to %d \n",n,n&mask);
-            if((n&mask)==2)count++;  
+            if((n&mask)==2)count++;
             n= n>>1;
         }
   return count;
-  
-    
+
+
 }
