219efec2ac5252c95206bd2bd453f272426403fc
Jad El mir <jadelmir@Jads-MacBook-Pro.local>
2019-02-25 14:11:07 -0500

    naming fixes and removal of commmented lines

--- my/myStrStr.c
+++ my/myStrStr.c
@@ -6,17 +6,13 @@ char *myStrStr(const char *haystack , const char *needle){
         char *what[10];
         if (*haystack == *needle) {
         strcpy(*what , haystack);
-
             while(*needle++ == *haystack++){
                 printf("WOOOOPSSSSSSS %c \n",*needle );
                 if(*needle == 0){printf("YEYYYYYY Result is %s\n" ,*what) ;
                  return *what;
-
-                
                 }
             }
         }
-        
         haystack++;
     }
 return (char *)haystack;
@@ -27,7 +23,7 @@ int main (){
     strcpy(a,"dogCat22");
     char *p = myStrStr(a,"Cat");
     // *p = 'x';
-    printf("p is %c \n and his refrence is %s",*p,p);
+    printf("p is %c \n and his reference is %s",*p,p);
     
 
 }
